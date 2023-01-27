eca3da68bef376068ab86072379362c793efc080
Nikolay Naumenko <nnv2006@ukr.net>
2023-01-27 21:04:35 +0200

    Відкорегував Options

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -29,7 +29,7 @@ class App extends Component {
     return (
       <Section title="Please leave feedback">
         <FeedbackOptions
-          options={['good', 'neutral', 'bad']}
+          options={Object.keys(this.state)}
           onLeaveFeedback={this.handleButtonClick}
         />
         <Statistic
